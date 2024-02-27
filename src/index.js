4dbaea776fa642289f6c0c93e1e00dba64179b13
xGaurav01 <gauravsharma76500@gmail.com>
2024-02-27 17:51:13 +0530

    Custom Api res and error handling and app config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error :", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`⚙️ Server is running at port :${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed !!!", err);
+  });
 
 // ( async () => {
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+export const asyncHandler =(reqhandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(reqhandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+// const asyncHandler=(func)=>async(req,res,next)=>{
+//     try{
+//         await func(req,res,next)
+//     }
+//     catch(err){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+
