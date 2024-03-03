9a04b9a8aef5921f57fc69973b69e15e559f4b8e
xGaurav01 <gauravsharma76500@gmail.com>
2024-03-03 17:23:31 +0530

    Added Refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -150,10 +150,9 @@ export const logOutUser = asyncHandler(async (req, res) => {
 
 export const refreshAccessToken = asyncHandler(async (req, res) => {
   try {
-    const incomingRefreshToken =
-      (await req.cookies.refreshToken) || req.body.refreshToken;
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
   
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
       throw new ApiError(401, "Unauthorized Request");
     }
   
